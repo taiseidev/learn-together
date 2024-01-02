7f9a47a7c1bdcc3a3153efd1eb84563491207384
onishi taisei <taisei.developer@gmail.com>
2024-01-02 23:13:33 +0900

    🚀 feat: ComposeQuadrant画面を作成

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -4,18 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -34,7 +42,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    TaskManager()
+                    ComposeQuadrant()
                 }
             }
         }
@@ -58,7 +66,7 @@ fun Body(title: String, subTitle: String, content: String, modifier: Modifier = 
 
 @Composable
 fun TaskManager(modifier: Modifier = Modifier) {
-    val image = painterResource(id = R.drawable.ic_task_completed)
+    val image = painterResource(id = R.drawable.ic_launcher_background)
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -76,11 +84,67 @@ fun TaskManager(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun ComposeQuadrant(modifier: Modifier = Modifier) {
+    Column(Modifier.fillMaxWidth()) {
+        Row(modifier = modifier.weight(1f)) {
+            Item(
+                backgroundColor = Color(0xFFEADDFF),
+                title = "Text composable",
+                content = "Displays text and follows the recommended Material Design guidelines.",
+                modifier = Modifier.weight(1f)
+            )
+            Item(
+                backgroundColor = Color(0xFFD0BCFF),
+                title = "Image composable",
+                content = "Creates a composable that lays out and draws a given Painter class object.",
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(modifier = modifier.weight(1f)) {
+            Item(
+                backgroundColor = Color(0xFFB69DF8),
+                title = "Row composable",
+                content = "A layout composable that places its children in a horizontal sequence.",
+                modifier = Modifier.weight(1f)
+            )
+            Item(
+                backgroundColor = Color(0xFFF6EDFF),
+                title = "Column composable",
+                content = "A layout composable that places its children in a vertical sequence.",
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+private fun Item(
+    backgroundColor: Color,
+    title: String,
+    content: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+    ) {
+        Text(
+            text = title,
+            modifier = modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = content, textAlign = TextAlign.Justify)
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LearnTogetherTheme {
-        TaskManager()
+        ComposeQuadrant()
 //        Body(
 //            stringResource(R.string.title),
 //            stringResource(R.string.sub_title),
