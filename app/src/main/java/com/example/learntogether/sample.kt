65a279e575c17dbc71381826740e930dda890d57
onishi taisei <taisei.developer@gmail.com>
2024-01-04 07:58:45 +0900

    🚀 feat: 条件追加

--- app/src/main/java/com/example/learntogether/sample.kt
+++ app/src/main/java/com/example/learntogether/sample.kt
@@ -1,14 +1,23 @@
 package com.example.learntogether
 
 fun main() {
-    val x: Any = 20;
+    val trafficLightColor = "Amber"
 
-    when (x) {
-        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
-        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
-        is Int -> println("x is an integer number, but not between 1 and 10.")
-        else -> println("x isn't a prime number between 1 and 10.")
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
     }
+
+//    val x: Any = 20;
+//
+//    when (x) {
+//        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+//        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+//        is Int -> println("x is an integer number, but not between 1 and 10.")
+//        else -> println("x isn't a prime number between 1 and 10.")
+//    }
 //    println(1 == 1)
 //    println(1 < 1)
 //    val trafficRightColor = "Black";
