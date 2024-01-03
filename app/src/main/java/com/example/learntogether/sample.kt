1fb13bf2dab1287b9d3eb74c893dd6fa896a1560
onishi taisei <taisei.developer@gmail.com>
2024-01-04 07:55:10 +0900

    🚀 feat: whenの条件でカンマ、in, isを使用した条件を追加

--- app/src/main/java/com/example/learntogether/sample.kt
+++ app/src/main/java/com/example/learntogether/sample.kt
@@ -1,14 +1,22 @@
 package com.example.learntogether
 
 fun main() {
-    println(1 == 1)
-    println(1 < 1)
-    val trafficRightColor = "Black";
+    val x: Any = 20;
 
-    when (trafficRightColor) {
-        "Red" -> println("Stop")
-        "Yellow" -> println("Slow")
-        "Green" -> println("Go")
-        else -> println("Invalid traffic-right color")
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
     }
+//    println(1 == 1)
+//    println(1 < 1)
+//    val trafficRightColor = "Black";
+//
+//    when (trafficRightColor) {
+//        "Red" -> println("Stop")
+//        "Yellow" -> println("Slow")
+//        "Green" -> println("Go")
+//        else -> println("Invalid traffic-right color")
+//    }
 }
