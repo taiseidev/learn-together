1d3ae8277f3deeeafc3325b18a4cae57b6bbcd9a
onishi taisei <taisei.developer@gmail.com>
2024-01-04 08:05:12 +0900

    when式に変更

--- app/src/main/java/com/example/learntogether/sample.kt
+++ app/src/main/java/com/example/learntogether/sample.kt
@@ -4,10 +4,12 @@ fun main() {
 
     val trafficLightColor = "Black"
 
-    val message = if (trafficLightColor == "Red") "Stop"
-    else if (trafficLightColor == "Yellow") "Slow"
-    else if (trafficLightColor == "Green") "Go"
-    else "Invalid traffic-light color"
+    val message = when (trafficLightColor) {
+        "Red" -> "Stop"
+        "Yellow" -> "Slow"
+        "Green" -> "Go"
+        else -> "Invalid traffic-light color"
+    }
 
     println(message)
 
