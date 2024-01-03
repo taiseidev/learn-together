da8da0c0593032763c4b243fafac557cc0309e05
onishi taisei <taisei.developer@gmail.com>
2024-01-04 07:47:38 +0900

    🚀 feat: whenステートメントに書き換え

--- app/src/main/java/com/example/learntogether/sample.kt
+++ app/src/main/java/com/example/learntogether/sample.kt
@@ -4,13 +4,11 @@ fun main() {
     println(1 == 1)
     println(1 < 1)
     val trafficRightColor = "Black";
-    if (trafficRightColor == "Red") {
-        println("Stop")
-    } else if (trafficRightColor == "Yellow") {
-        println("Slow")
-    } else if (trafficRightColor == "Green") {
-        println("Go")
-    } else {
-        println("Invalid traffic-right color")
+
+    when (trafficRightColor) {
+        "Red" -> println("Stop")
+        "Yellow" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-right color")
     }
 }
